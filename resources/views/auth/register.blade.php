cdb0df33ff4ff24ead7bd3d0f92140a8b05ef20f
KABIR <md.kabirhassan2009@gmail.com>
2024-02-14 03:18:10 +0600

    Update code

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.master-auth')
 
 @push('plugin-styles')
+<<<<<<< HEAD
 {!! Html::style('assets/css/loader.css') !!}
 {!! Html::style('plugins/owl-carousel/owl.carousel.min.css') !!}
 {!! Html::style('plugins/owl-carousel/owl.theme.default.min.css') !!}
@@ -32,20 +33,61 @@
                             <div class="login-one-inputs mt-3">
                                 <input id="email" class="form-control @error('email') is-invalid @enderror" type="email" value="{{ old('email') }}" placeholder="{{__('Email')}}" />
                                 <i class="las la-lock"></i>
+=======
+    <!-- Add any additional styles for the registration page here -->
+@endpush
+
+@section('content')
+    <!-- Main Body Starts -->
+    <div class="register">
+        <div class="container-fluid register-container">
+            <div class="p-30 h-100">
+                <div class="row main-register h-100">
+                    <div class="col-xl-4 col-lg-6 col-md-6 col-sm-12 p-0">
+                        <div class="register-start">
+                            <h6 class="mt-2 text-primary text-center font-20">{{ __('Sign Up') }}</h6>
+                            <p class="text-center text-muted mt-3 mb-3 font-14">{{ __('Create a new account') }}</p>
+                            <form method="POST" action="{{ route('register') }}">
+                                @csrf
+
+                                <div class="register-inputs mt-5">
+                                    <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" placeholder="Your Name" required autocomplete="name" autofocus>
+                                    <i class="las la-user"></i>
+                                </div>
+                                @error('Team name')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+
+                                <div class="register-inputs mt-3">
+                                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" placeholder="Email" required autocomplete="email">
+                                    <i class="las la-envelope"></i>
+                                </div>
+>>>>>>> a2b10a1ebc0cc1aa739ccd406e74467aa71388cb
                                 @error('email')
                                 <span class="invalid-feedback" role="alert">
                                     <strong>{{ $message }}</strong>
                                 </span>
                                 @enderror
+<<<<<<< HEAD
                             </div>
                             <div class="login-one-inputs mt-3">
                                 <input id="password" class="form-control @error('password') is-invalid @enderror" type="password" placeholder="{{__('Password')}}" />
                                 <i class="las la-lock"></i>
+=======
+
+                                <div class="register-inputs mt-3">
+                                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" placeholder="Password" required autocomplete="new-password">
+                                    <i class="las la-lock"></i>
+                                </div>
+>>>>>>> a2b10a1ebc0cc1aa739ccd406e74467aa71388cb
                                 @error('password')
                                 <span class="invalid-feedback" role="alert">
                                     <strong>{{ $message }}</strong>
                                 </span>
                                 @enderror
+<<<<<<< HEAD
                             </div>
                             <div class="login-one-inputs mt-3">
                                 <input id="password-confirm" type="password" name="password_confirmation" placeholder="{{__('Confirm Password')}}" />
@@ -119,10 +161,31 @@
                             </div>
                         </div>
                     </div>
+=======
+
+                                <div class="register-inputs mt-3">
+                                    <input id="password-confirm" type="password" class="form-control" name="password_confirmation" placeholder="Confirm Password" required autocomplete="new-password">
+                                    <i class="las la-lock"></i>
+                                </div>
+
+                                <div class="register-inputs mt-4">
+                                    <button class="ripple-button ripple-button-primary btn-lg btn-register" type="submit">
+                                        <div class="ripple-ripple js-ripple">
+                                            <span class="ripple-ripple__circle"></span>
+                                        </div>
+                                        {{ __('REGISTER') }}
+                                    </button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                    <!-- You can add additional content for the registration page here -->
+>>>>>>> a2b10a1ebc0cc1aa739ccd406e74467aa71388cb
                 </div>
             </div>
         </div>
     </div>
+<<<<<<< HEAD
 </div>
 <!-- Main Body Ends -->
 @endsection
@@ -139,3 +202,11 @@
 @push('custom-scripts')
 
 @endpush
+=======
+    <!-- Main Body Ends -->
+@endsection
+
+@push('plugin-scripts')
+    <!-- Add any additional scripts for the registration page here -->
+@endpush
+>>>>>>> a2b10a1ebc0cc1aa739ccd406e74467aa71388cb
